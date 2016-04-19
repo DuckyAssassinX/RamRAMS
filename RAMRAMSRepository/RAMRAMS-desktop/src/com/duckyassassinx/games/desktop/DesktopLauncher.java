dc8fff647d4efd1f06999c68eae55339135a4162
Nathan <duckyxyz@gmail.com>
2016-04-18 20:50:42 -0500

    Initial Checkin For libgdx project

---
+++ RAMRAMSRepository/RAMRAMS-desktop/src/com/duckyassassinx/games/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.duckyassassinx.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.duckyassassinx.games.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+		
+		//Nathan
+	}
+}
